5a7ef50d69b4846695c29882405dc6b29cbe5b88
Андрей Казбеков <kazbekovandrew@gmail.com>
2026-02-03 22:33:04 +0300

    Определены поля игрового персонажа в классе GameCharacter.java и внесены правки в класс Entity.java

--- IceAndFire/src/Entities/Entity.java
+++ IceAndFire/src/Entities/Entity.java
@@ -1,4 +1,46 @@
 package Entities;
 
-public class Entity {
+public abstract class Entity {
+
+    private final String name; // Имя сущности
+    private int hp; // Уровень здоровья
+    private int maxHp; // Максимальный уровень здоровья
+    private int power; // Сила
+    private int dexterity; // Ловкость
+    private int level; // Уровень сущности
+
+    /*
+    * Конструктор сущности по умолчанию.
+    * */
+    public Entity(String name) {
+        this.name = name;
+        this.hp = 100;
+        this.maxHp = hp;
+        this.power = 5;
+        this.dexterity = 50;
+        this.level = 1;
+    }
+
+    /*
+    * Конструктор для пользовательской конфигурации сущности (используется для создания монстров)
+    * */
+    public Entity(String name, int hp, int maxHp, int power, int dexterity, int level) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = maxHp;
+        this.power = power;
+        this.dexterity = dexterity;
+        this.level = level;
+    }
+
+    /*
+     * Метод нанесения урона: в качестве аргумента принимает сущность, которую атакуют.
+     * Возвращает значение, сколько на самом деле было нанесено урона.
+     * */
+    public abstract int dealDamage(Entity anotherEntity);
+
+    /*
+     * Метод получения урона. Возвращает значение, сколько на самом деле было получено урона.
+     * */
+    public abstract int takeDamage();
 }
--- IceAndFire/src/Entities/GameCharacter.java
+++ IceAndFire/src/Entities/GameCharacter.java
@@ -1,4 +1,58 @@
 package Entities;
 
-public class GameCharacter {
+import Items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameCharacter extends Entity {
+
+    /*
+    * Текущий накопленный опыт. Обнуляется при достижении нового уровня
+    * */
+    private int currentExperience = 0;
+
+    /*
+    * Потолок накопления опыта. После достижения этой границы увеличивается уровень
+    * */
+    private int totalExperience = 100;
+
+    /*
+    * Ведущая рука. Здесь хранится предмет, который персонаж держит прямо сейчас. Это может быть меч, либо иной предмет
+    * */
+    private Item leadingHand = null;
+
+    /*
+    * Рюкзак персонажа. В нем он может складывать вещи
+    * */
+    private List<Item> backpack = new ArrayList<>();
+
+    /*
+    * Лимит предметов в рюкзаке. По умолчанию 8
+    * */
+    private int backpackLimit = 8;
+
+    /*
+    * Количество предметов в рюкзаке на текущий момент
+    * */
+    private int currentBackpackSize = 0;
+
+    /*
+    * Монеты персонажа. На них он может покупать у торговца амуницию
+    * */
+    private int coins = 120;
+
+    public GameCharacter(String name) {
+        super(name);
+    }
+
+    @Override
+    public int dealDamage(Entity anotherEntity) {
+        return 0;
+    }
+
+    @Override
+    public int takeDamage() {
+        return 0;
+    }
 }
