38ee2e7e1729ebde361ea713b0d90bc1ae4f17da
Андрей Казбеков <kazbekovandrew@gmail.com>
2026-02-05 15:39:30 +0300

    В класс Weapon.java добавлено новое поле price: Int

--- IceAndFire/src/Items/Item.java
+++ IceAndFire/src/Items/Item.java
@@ -2,13 +2,26 @@ package Items;
 
 public abstract class Item {
 
+    /*
+    * Название предмета
+    * */
     private final String title;
 
-    public Item(String title) {
+    /*
+    * Стоимость предмета
+    * */
+    private final int price;
+
+    public Item(String title, int price) {
         this.title = title;
+        this.price = price;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
--- IceAndFire/src/Items/Weapon.java
+++ IceAndFire/src/Items/Weapon.java
@@ -2,12 +2,23 @@ package Items;
 
 public abstract class Weapon extends Item {
 
+    /*
+    * Базовый урон наносимый этим оружием
+    * */
     private int power;
+
+    /*
+    * Описание этого оружия
+    * */
     private String description;
+
+    /*
+    * Код для проигрывания определенных реплик связанных с использованием игрока этого оружия
+    * */
     private int replicaCode;
 
-    public Weapon(String title, int power, String description, int replicaCode) {
-        super(title);
+    public Weapon(String title, int power, String description, int replicaCode, int price) {
+        super(title, price);
         this.power = power;
         this.description = description;
         this.replicaCode = replicaCode;
